ae6c78c65d4931359ca314c5a35c0762a56cbcb9
AlvaroJC17 <alvarocortesia@gmail.com>
2023-12-03 15:16:15 -0300

    se soluciono el problema de las pestanas del nav del portal admin, aparece el arror del nombre en la pagina homeAdmin

--- Estetica/src/main/java/com/proyecto_integrador_3/Estetica/Controllers/ControladorAdmin.java
+++ Estetica/src/main/java/com/proyecto_integrador_3/Estetica/Controllers/ControladorAdmin.java
@@ -107,61 +107,75 @@ public class ControladorAdmin {
 	@GetMapping("/listarUsuariosVisibles")
     public String listarUsuariosVisibles(@RequestParam(name = "email") String email, ModelMap model) throws MiExcepcion {
 		List<Usuario> usuarios = repositorioUsuario.joinUsuarioPersona();
-		List<Usuario> usuariosEmail = repositorioUsuario.buscarPorEmail(email);
-		model.addAttribute("usuariosEmail", usuariosEmail);
+		model.addAttribute("usuariosEmail", email);
 		model.addAttribute("usuarios", usuarios);
 		return "/pagina_admin/portalAdmin";
 	}
 	
 	//Muestra una pagina con la lista de usuarios oculta, esto porque queremos que el admin
 	//busque los usuarios por nombre, dni o email y no que los busque en una lista
-	@GetMapping("/listarUsuarios")
-    public String listarUsuarios(@RequestParam(name = "email") String email, Model model) throws MiExcepcion {
-		List<Usuario> usuariosEmail = repositorioUsuario.buscarPorEmail(email);
-		model.addAttribute("usuariosEmail", usuariosEmail);
+	@GetMapping("/listarUsuariosOcultos")
+    public String listarUsuarios(
+    		@RequestParam(name = "email") String email,
+    		@RequestParam(name = "exito", required = false) String exito, //El requiered=false le indica al metodo que el valor de esta variable es opcional y puede llegar null, asi no se rempe el programa
+    		@RequestParam(name = "error", required = false) String error,
+    		Model model) throws MiExcepcion {
+
+		//List<Usuario> usuariosEmail = repositorioUsuario.buscarPorEmail(email);
+		model.addAttribute("usuariosEmail", email); //Con este mail mostramos el nav en este metodo
+		model.addAttribute("exito", exito); //inyectamos mensaje de exito si es necesario
+		model.addAttribute("error", error); //inyectamos mensaje de error si es necesario
 		return "/pagina_admin/portalAdmin";
 	}
-		
-
-	//Buscamos usuario por dni, nombre o email
+	
+	//Buscamos usuario por dni, nombre o email, la variable emailAdmin es para pasar el mail de admin y poder visualizar el nav
 	@PostMapping("/buscarDNIoNombre")
-	public String buscarDniNombreEmail(@RequestParam(name = "dato") String dato, Model model) {
+	public String buscarDniNombreEmail(@RequestParam(name = "dato") String dato, @RequestParam(name = "emailAdmin") String emailAdmin, Model model) {
 
 		//Validamos que la busqueda no se haya hecho en blanco y mostramos un mensaje
 		if (dato.isEmpty() || dato == null) {
 			model.addAttribute("error", "Indique un numero de DNI, nombre o email para realizar la busqueda");
+			model.addAttribute("usuariosEmail", emailAdmin);
 			return "/pagina_admin/portalAdmin";
 		}
-		
+					
 		List<Usuario> usuarioDni = servicioUsuario.buscarDni(dato);
 		List<Usuario>	usuarioNombre = servicioUsuario.buscarNombre(dato);
 		List<Usuario> usuarioEmail = servicioUsuario.buscarPorEmail(dato);
 		
 		if (!usuarioDni.isEmpty()) {
-			model.addAttribute("usuarios", usuarioDni); // asignamos el valor de la variable administradoresDni a la variable html administradores y asi poder iterarla en el documento 
+			model.addAttribute("usuarios", usuarioDni); // asignamos el valor de la variable administradoresDni a la variable html administradores y asi poder iterarla en el documento
+			model.addAttribute("usuariosEmail", emailAdmin);
+			return "/pagina_admin/portalAdmin";
 		}	
 		if (!usuarioNombre.isEmpty()) {
 			model.addAttribute("usuarios", usuarioNombre);
+			model.addAttribute("usuariosEmail", emailAdmin);
+			return "/pagina_admin/portalAdmin";
 		}	
 		if (!usuarioEmail.isEmpty()) {
 			model.addAttribute("usuarios", usuarioEmail);
+			model.addAttribute("usuariosEmail", emailAdmin);
+			return "/pagina_admin/portalAdmin";
 		}
+		model.addAttribute("usuariosEmail", emailAdmin);
 		return "/pagina_admin/portalAdmin";
 	}
 		
-	
-	
 	
 	// En este metodo unificamos la edicion y eliminacion de un usuario a traves de un solo formulario
 	// usando el action como valor para las diferentes condiciones
 	@PostMapping("/modificarUsuario")
 	public String modificarUsuario(@RequestParam(name = "idUsuario", required = false) String id,
-            @RequestParam(name = "nuevoRol") String nuevoRolNombre, @RequestParam(name = "action") String action,
-            Model model) {
+								   @RequestParam(name = "nuevoRol") String nuevoRolNombre,
+            					   @RequestParam(name = "emailAdministrador") String emailAdministrador,
+            					   @RequestParam(name = "action") String action,
+            					   Model model) {
 		
 		//Validamos que el id no venga vacio, esto pasa cuando le dan a una opcion sin seleccionar a un usuario
 		if (id == null) {
 			model.addAttribute("error", "Es necesario seleccionar un usuario!!");
+			model.addAttribute("usuariosEmail", emailAdministrador);
 			return "/pagina_admin/portalAdmin";
 		}
 		
@@ -171,7 +185,6 @@ public class ControladorAdmin {
 			try {
 	            // Convertir el nuevoRol solo si no es "Modificar Rol"
 	            nuevoRol = Rol.valueOf(nuevoRolNombre.toUpperCase());
-	            
 	        } catch (IllegalArgumentException e) {
 	            System.out.println(e.getMessage());
 	        }
@@ -188,7 +201,7 @@ public class ControladorAdmin {
 	     //Ya teniendo el mail de usuario puedo inyectarlo en el resultado de cada if
 		// para que ademas de cumplir la funcion de cada condicional tambien la pag
 		// me recargue con la barra de nav. Recordar que como la barra de nav este en un
-		//bucle, sino le paso un valor de mail esta no se me activo y no puedo verla
+		//condicional, sino le paso un valor de mail esta no se me activo y no puedo verla
 		if (action.equals("modificarRol") && nuevoRol != null) {
 			//metodo para moficar el rol
 			servicioUsuario.modificarRol(id, nuevoRol);
@@ -223,11 +236,11 @@ public class ControladorAdmin {
 			try {
 				servicioUsuario.borrarUsuario(id);
 				servicioPersona.borrarPersona(id);
-				model.addAttribute("exito2", "Usuario eliminado correctamente!!");
-				return "redirect:/listarUsuarios?email=" + emailUsuario;
+				String exito = "Usuario eliminado correctamente";
+				return "redirect:/listarUsuariosOcultos?email=" + emailUsuario + "&exito=" + exito; //Enviamos mail y mensaje de exito
 			} catch (Exception e) {
-				model.addAttribute("error", "No se pudo borrar el usuario");
-				return "redirect:/listarUsuarios?email=" + emailUsuario;
+				String error = "Usuario no se pudo eliminar";
+				return "redirect:/listarUsuariosOcultos?email=" + emailUsuario + "&error=" + error; //Enviamos mail y mensaje de error;
 			}
 		}
 		return "/pagina_admin/portalAdmin";
@@ -239,8 +252,7 @@ public class ControladorAdmin {
 	@GetMapping("/listarUsuariosPorId")
 	public String listarUsuariosPorId(String id, String email, Model model) {
 		List<Usuario> usuarios = servicioUsuario.buscarId(id);
-		List<Usuario> usuariosEmail = repositorioUsuario.buscarPorEmail(email);
-		model.addAttribute("usuariosEmail", usuariosEmail);
+		model.addAttribute("usuariosEmail", email);
 		model.addAttribute("usuarios", usuarios);
 		model.addAttribute("exito", "Modificación realizada exitosamente!!");
 		return "/pagina_admin/portalAdmin";
--- Estetica/src/main/java/com/proyecto_integrador_3/Estetica/Controllers/ControladorPagina.java
+++ Estetica/src/main/java/com/proyecto_integrador_3/Estetica/Controllers/ControladorPagina.java
@@ -51,23 +51,21 @@ public class ControladorPagina {
 	
 	// Registro un usuario en la base de datos
 	 @PostMapping("/registro")
-	    public String registroUsuario(@RequestParam(name = "email") String email, @RequestParam(name = "contrasena") String password,
-	           @RequestParam(name = "contrasena2") String password2, ModelMap modelo) throws MiExcepcion {
+	    public String registroUsuario(@RequestParam(name = "email") String email,
+	    							  @RequestParam(name = "contrasena") String password,
+	    							  @RequestParam(name = "contrasena2") String password2,
+	    							  ModelMap modelo) throws MiExcepcion {
 
-		
-	        try {
-
+		 try {
 	        	servicioUsuario.guardarUsuario(email, password, password2);
 	        	return "index.html";
-	        
 	        } catch (MiExcepcion e) {
 	            System.out.println(e.getMessage());
 	            modelo.put("error", e.getMessage());
 	            return "registrarse";
 	        }
+	 }
 
-	    }
-	 
 	 	//Login de un usuario, dependiendo de su rol lo redireccion a un home y si es logueo
 	 	// por primera vez lo redirecciona a una formulario de datos
 		@PostMapping("/loginUsuario")
@@ -76,33 +74,31 @@ public class ControladorPagina {
 		         @RequestParam(name = "password") String contrasena,
 		         ModelMap model, Model modelo) throws MiExcepcion {
 
-			 String password = null;
 		     Optional<Usuario> usuarioOptional = servicioUsuario.buscarPorEmailOptional(email);
 
 		     if (usuarioOptional.isPresent()) {
 		    	 
 		         Usuario usuario = usuarioOptional.get(); //con el metodo get, asociamos al usuario encontrado a la variable usuario y asi poder acceder a sus atributos
 		         String emailUsuario = usuario.getEmail();
-		         password = usuario.getContrasena();
+		         String password = usuario.getContrasena();
 		         Boolean activo = usuario.getActivo();
 		         Boolean validarForm = usuario.getValidacionForm();
 		         Rol rol = usuario.getRol();
 		         
+		         //Verificamos que el usuario este activo
 		         if (!activo) {
 						model.addAttribute("Error", "Usuario no se encuentra activo");
 						return "login";
 					}
 			         
-		      
 		         try {
-		        	 //verificamos que los password esten correcto y sean iguales ademas de que el usuario este activo
+		        	 //verificamos que los password esten correcto y sean iguales
 		        	  if (servicioUsuario.verificarPassword(contrasena, password)) {
 		        		  
-		        		  //Buscamos los datos pertenecientes a ese email en la table de persona y los pasamos a homeAdmin, en este caso solo usamos el nombre
+		        		  //Buscamos los datos pertenecientes a ese email en la tabla de persona y los pasamos a homeAdmin, en este caso solo usamos el nombre
 		        		  List <Usuario> datosPersonaUsuario = servicioUsuario.buscarPorEmail(emailUsuario);
 		        		  
 				             if ("ADMIN".equals(rol.toString())) {
-				       
 				            	 if (validarForm) {
 				            		 modelo.addAttribute("datosAdmin", datosPersonaUsuario);
 				            		 return "pagina_admin/homeAdmin";
@@ -110,10 +106,7 @@ public class ControladorPagina {
 				            		 modelo.addAttribute("emailUsuario", emailUsuario);
 				            		 return "completarDatos";
 				            	 }
-									
-				            	 
 				             } else if ("CLIENTE".equals(rol.toString())) {
-				            	
 				            	 if (validarForm) {
 				            		 modelo.addAttribute("datosCliente", datosPersonaUsuario);
 				            		 return "pagina_cliente/homeCliente";
@@ -121,9 +114,7 @@ public class ControladorPagina {
 				            		 modelo.addAttribute("emailUsuario", emailUsuario); //Esta variable envia el email en un input oculto hacia el metodo guardarDatosPersona
 				            		 return "completarDatos";
 				            	 }
-				            	 
-				             } else if ("PROFESIONAL".equals(rol.toString())) {
-				            	 
+				             } else if ("PROFESIONAL".equals(rol.toString())) {				            	 
 				            	 if (validarForm) {
 				            		 modelo.addAttribute("datosProfesional", datosPersonaUsuario);
 				            		 return "pagina_profesional/homeProfesional";
